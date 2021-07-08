df32eb36e1861f9eebfcb8e9585eb86e03db9ac8
NiutaR <69516148+NiutaR@users.noreply.github.com>
2021-07-08 23:00:50 +0300

    done

---
+++ .history/trainigDays_20210708225831.js
@@ -0,0 +1,49 @@
+// The scope of `random` is too loose 
+
+ 
+const getRandEvent = () => {
+  const random = Math.floor(Math.random() * 3);
+  if (random === 0) {
+    return 'Marathon';
+  } else if (random === 1) {
+    return 'Triathlon';
+  } else if (random === 2) {
+    return 'Pentathlon';
+  }
+};
+//console.log(getRandEvent());
+ 
+// The scope of `days` is too tight 
+const getTrainingDays = event => {
+
+  let days = '';
+  if (event === 'Marathon') {
+    let days = 50;
+  } else if (event === 'Triathlon') {
+    let days = 100;
+  } else if (event === 'Pentathlon') {
+    let days = 200;
+  }
+ 
+  return days;
+};
+ 
+// The scope of `name` is too tight 
+const logEvent = (name, event) => {
+  const name = 'Nala';
+  console.log(`${name}'s event is: ${event}`);
+};
+ 
+const logTime = (name, days) => {
+  
+  console.log(`${name}'s time to train is: ${days} days`);
+};
+ 
+
+const event = getRandEvent();
+const days = getTrainingDays(event);
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
+ 
+ 
+logEvent(event);
+logTime(days);
